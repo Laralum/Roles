69bdd7b697522fd86d9d4b8a9182de948f2deb4a
Erik Campobadal <soc@erik.cat>
2017-02-21 23:18:00 +0100

    Tweaks

--- src/Views/permissions.blade.php
+++ src/Views/permissions.blade.php
@@ -12,8 +12,8 @@
 @section('content')
     <div class="uk-container uk-container-large">
         <div uk-grid>
-            <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
-            <div class="uk-width-1-1@s uk-width-3-5@l uk-width-1-3@xl">
+            <div class="uk-width-1-1@s uk-width-1-5@l"></div>
+            <div class="uk-width-1-1@s uk-width-3-5@l">
                 <div class="uk-card uk-card-default">
                     <div class="uk-card-header">
                         @lang('laralum_roles::general.available_permissions')
@@ -23,30 +23,30 @@
                             {{ csrf_field() }}
                             @if(isset($method)) {{ method_field($method) }} @endif
                             <fieldset class="uk-fieldset">
-                                   <div class="uk-margin uk-grid">
-                                   @foreach($permissions as $permission)
-                                       <div class="uk-width-1-1@m uk-width-1-2@l uk-margin-small-bottom">
-                                           <label><input class="uk-checkbox" name="{{ $permission->id }}" type="checkbox"  @if($role->hasPermission($permission)) checked @endif> {{ $permission->name }}</label>
-                                           <div class="uk-text-meta">
-                                               ({{ $permission->slug }})
-                                               <br>
-                                               {{ $permission->description }}
+                                   <div uk-grid>
+                                       @foreach($permissions as $permission)
+                                           <div class="uk-width-1-1@m uk-width-1-2@l">
+                                               <label><input class="uk-checkbox" name="{{ $permission->id }}" type="checkbox"  @if($role->hasPermission($permission)) checked @endif> {{ $permission->name }}</label>
+                                               <div class="uk-text-meta">
+                                                   {{ $permission->description }}
+                                               </div>
                                            </div>
-                                       </div>
-                                   @endforeach
+                                       @endforeach
                                    </div>
                                    <div class="uk-margin">
-                                       <a href="{{ route('laralum::users.index') }}" class="uk-button uk-button-default uk-align-left"> @lang('laralum_roles::general.cancel')</a>
-                                       <button type="submit" class="uk-button uk-button-primary uk-align-right">
-                                           <span class="ion-forward"></span>&nbsp; @lang('laralum_roles::general.submit')
-                                       </button>
+                                       <a href="{{ route('laralum::roles.index') }}" class="uk-button uk-button-default"> @lang('laralum_roles::general.cancel')</a>
+                                       <div class="uk-align-right">
+                                           <button type="submit" class="uk-button uk-button-primary">
+                                               <span class="ion-forward"></span>&nbsp; @lang('laralum_roles::general.submit')
+                                           </button>
+                                       </div>
                                    </div>
                                </fieldset>
                         </form>
                     </div>
                 </div>
             </div>
-            <div class="uk-width-1-1@s uk-width-1-5@l uk-width-1-3@xl"></div>
+            <div class="uk-width-1-1@s uk-width-1-5@l"></div>
         </div>
     </div>
 @endsection
