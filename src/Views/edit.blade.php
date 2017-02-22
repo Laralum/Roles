fdbade423eccbbc264bb4b224c82d702d8896b0e
24aitor <1aitorribaarchilla@gmail.com>
2017-02-22 16:44:04 +0100

    Fixed edit view breadcrumb

--- src/Views/edit.blade.php
+++ src/Views/edit.blade.php
@@ -2,6 +2,13 @@
 @section('icon', 'ion-edit')
 @section('title', __('laralum_roles::general.edit_role'))
 @section('subtitle', __('laralum_roles::general.edit_desc', ['id' => $role->id, 'time_ago' => $role->created_at->diffForHumans()]))
+@section('breadcrumb')
+    <ul class="uk-breadcrumb">
+        <li><a href="{{ route('laralum::index') }}">@lang('laralum_roles::general.home')</a></li>
+        <li><a href="{{ route('laralum::roles.index') }}">@lang('laralum_roles::general.role_list')</a></li>
+        <li><span>@lang('laralum_roles::general.create_role')</span></li>
+    </ul>
+@endsection
 @section('content')
     @include('laralum_roles::form', [
         'action' => route('laralum::roles.update', ['role' => $role]),
