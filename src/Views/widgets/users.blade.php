47cbafbefa35a9737a735d795af0f71bc44cb161
Erik Campobadal <soc@erik.cat>
2017-04-11 15:33:07 +0200

    Widgets

---
+++ src/Views/widgets/users.blade.php
@@ -0,0 +1,25 @@
+@php
+    $roles = \Laralum\Roles\Models\Role::all();
+    $values = $roles->map(function($role) {
+        return $role->users->count();
+    })->toArray();
+@endphp
+@if ( (count($values) > 0) and (collect($values)->sum() == 0) )
+    <div style="height: 400px; position: relative;">
+        <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <center>
+                <i class="ion-sad-outline" style="font-size: 100px;"></i><br />
+                <span>@lang('laralum_roles::general.no_data')</span>
+            </center>
+        </div>
+    </div>
+@else
+    {!!
+        \ConsoleTVs\Charts\Facades\Charts::create('pie', 'highcharts')
+            ->title(__('laralum_roles::general.role_users_c'))->dimensions(0, 400)
+            ->labels($roles->pluck('name')->toArray())
+            ->values($values)
+            ->colors($roles->pluck('color')->toArray())
+            ->render()
+    !!}
+@endif
